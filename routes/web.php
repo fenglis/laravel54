effbf24f40d8cdcccfdc1756377111379ded65e5
root <root@myserver.(none)>
2018-01-23 12:41:51 +0800

    add fans and stars

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingSto
 
 
 
+
 //文章模块
 //文章列表
 Route::get('/posts', '\App\Http\Controllers\PostController@index');
@@ -53,3 +54,9 @@ Route::post('/posts/{post}/comment', '\App\Http\Controllers\PostController@comme
 Route::get('/posts/{post}/zan', '\App\Http\Controllers\PostController@zan');
 //取消赞
 Route::get('/posts/{post}/unzan', '\App\Http\Controllers\PostController@unzan');
+
+
+//个人中心
+Route::get('/user/{user}', '\App\Http\Controllers\UserController@show');
+Route::post('/user/{user}/fan', '\App\Http\Controllers\UserController@fan');
+Route::post('/user/{user}/unfan', '\App\Http\Controllers\UserController@unfan');
